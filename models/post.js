48c198a5bc7c3615711a12bffb63282980d96cf7
Jerry Pao <kyjerry0424@gmail.com>
2023-04-23 15:14:30 -0400

    define mongoose post schema

---
+++ models/post.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+const  postSchema = new Schema({
+    topic: {
+        type: String,
+        required: true,
+    },
+    photo: {
+        type: String,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: function() {
+            let date = new Date()
+            return date
+        }
+    },
+    location: {
+        type: String,
+    },
+    author: {
+        type: Schema.Types.ObjectId, ref: "Profile"
+    },
+    // comments: [commentSchema]
+},{
+    timestamps: true
+})
