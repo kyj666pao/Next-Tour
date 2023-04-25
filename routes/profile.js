d2b7436eee29581a3635ac9de45c57bcd0c486cd
Jerry Pao <kyjerry0424@gmail.com>
2023-04-25 11:50:19 -0400

    define the route to all profile and create the view

---
+++ routes/profile.js
@@ -0,0 +1,44 @@
+import { Router } from "express";
+import { isLoggedIn } from "../middleware/middleware.js"
+const router = Router()
+
+import { Profile } from "../models/profile.js"
+
+//GET "/profile" profile.index
+router.get("/", isLoggedIn, (req,res) => {
+    // let { profileId } = req.params
+//  Profile.findById(profileId)
+Profile.find()
+    .then(profiles => {
+        // res.send("this is profile")
+        res.render("profiles/index", {
+            profiles,
+            title: "Profile"
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.redirect("/")
+    })
+})
+
+//GET "/profile/:profileId"
+router.get("/:profileId", isLoggedIn, (req,res) => {
+    let { profileId } = req.params
+    Profile.findById(profileId)
+    .then(profile => {
+        const isSelf = profile._id.equals(req.user.profile._id)
+            res.render("profiles/show", {
+                profile,
+                isSelf,
+                title: `${profile.name}'s profile`
+            })
+    })
+    .catch(err => {
+        console.log(err)
+        // res.redirect("/")
+        res.send("profile is not found")
+    })
+})
+
+export { router}
--- server.js
+++ server.js
@@ -22,6 +22,8 @@ import'./config/passport.js'
 import { router as indexRouter } from './routes/index.js'
 import { router as authRouter } from './routes/auth.js'
 import { router as postsRouter } from './routes/posts.js'
+import { router as profileRouter } from './routes/profile.js'
+
 
 // create the express app
 const app = express()
@@ -63,6 +65,7 @@ app.use(passDataToView)
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
 app.use("/posts", postsRouter)
+app.use("/profile", profileRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
